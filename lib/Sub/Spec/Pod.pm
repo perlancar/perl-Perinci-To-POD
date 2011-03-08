11d28f7aa2ef5b3c9b779aafbe24a312c3c4574e
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-03-08 15:09:36 +0700

    parse result_naked

--- lib/Sub/Spec/Pod.pm
+++ lib/Sub/Spec/Pod.pm
@@ -29,8 +29,10 @@ sub _gen_sub_pod($;$) {
     die "No name in spec" unless $sub_spec->{name};
     $log->trace("Generating POD for $sub_spec->{name} ...");
 
+    my $naked = $sub_spec->{result_naked};
+
     $pod .= "=head2 $sub_spec->{name}(\%args) -> ".
-        "[STATUSCODE, ERRMSG, RESULT]\n\n";
+        ($naked ? "RESULT" : "[STATUS_CODE, ERR_MSG, RESULT]")."\n\n";
 
     if ($sub_spec->{summary}) {
         $pod .= "$sub_spec->{summary}.\n\n";
@@ -42,12 +44,16 @@ sub _gen_sub_pod($;$) {
         $pod .= "$desc\n\n";
     }
 
-    $pod .= <<'_';
-Returns a 3-element arrayref. STATUSCODE is 200 on success, or an error code
-between 3xx-5xx (just like in HTTP). ERRMSG is a string containing error
+    if ($naked) {
+
+    } else {
+        $pod .= <<'_';
+Returns a 3-element arrayref. STATUS_CODE is 200 on success, or an error code
+between 3xx-5xx (just like in HTTP). ERR_MSG is a string containing error
 message, RESULT is the actual result.
 
 _
+    }
 
     my $args  = $sub_spec->{args} // {};
     $args = { map {$_ => _parse_schema($args->{$_})} keys %$args };
@@ -178,7 +184,7 @@ __END__
 This module generates API POD documentation for all subs in specified module.
 Example output:
 
- =head2 sub1(%args) -> [STATUSCODE, ERRORMSG, RESULT]
+ =head2 sub1(%args) -> [STATUS_CODE, ERR_MSG, RESULT]
 
  Summary of sub1.
 
@@ -198,7 +204,7 @@ Example output:
 
  =back
 
- =head2 sub2(%args) -> [STATUSCODE, ERRMSG, RESULT]
+ =head2 sub2(%args) -> [STATUS_CODE, ERR_MSG, RESULT]
 
  ...
 
